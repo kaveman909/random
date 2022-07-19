dcabd6334efd9ec890c149cb14196d17c70b8a72
David Tracy <david.tracy3@gmail.com>
2022-07-18 21:56:51 -0700

    init commit

---
+++ inline-outline-merger.py
@@ -0,0 +1,60 @@
+#!/opt/homebrew/bin/python3
+
+import os
+import sys
+
+class Transcript:
+  def __init__(self, timestamp, data, type):
+    self.timestamp = float(timestamp[1:-1])
+    self.data = data
+    self.type = type
+
+
+folder = sys.argv[1]
+INLINE_SUFFIX = "_inLine.txt"
+OUTLINE_SUFFIX = "_outLine.txt"
+
+inline_files = [x[:-len(INLINE_SUFFIX)]
+                for x in os.listdir(folder) if INLINE_SUFFIX in x]
+outline_files = [x[:-len(OUTLINE_SUFFIX)]
+                 for x in os.listdir(folder) if OUTLINE_SUFFIX in x]
+
+inline_files.sort()
+outline_files.sort()
+
+def parse_transcripts(lines, transcripts, type):
+  timestamp = ''
+  data = []
+  for line in lines:
+    if line.startswith('[') and line.endswith(']'):
+      if timestamp:
+        transcripts.append(Transcript(timestamp, data, type))
+        data = []
+      timestamp = line
+    else:
+      data.append(line)
+  if timestamp and data:
+    transcripts.append(Transcript(timestamp, data, type))
+
+for inline in inline_files:
+  if inline not in outline_files:
+    continue
+  else:
+    # found a valid pair of inline/outline
+    print(inline)
+    inline_lines = [x.strip() for x in open(os.path.join(
+        folder, inline + INLINE_SUFFIX), 'r').readlines()]
+    outline_lines = [x.strip() for x in open(os.path.join(
+        folder, inline + OUTLINE_SUFFIX), 'r').readlines()]
+    transcripts = []
+    parse_transcripts(inline_lines, transcripts, 'inLine')
+    parse_transcripts(outline_lines, transcripts, 'outLine')
+
+    transcripts.sort(key=lambda x: x.timestamp)
+    for transcript in transcripts:
+      row = '{}\t{}'.format(transcript.type, transcript.timestamp)
+      for data in transcript.data:
+        row += '\t' + data
+      print(row)
+    print('\n\n\n')
+    
